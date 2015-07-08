8e27d4b4e6efde7222a3cb9db9c5931d4c2c9ac8
soraver <soraver@users.noreply.github.com>
2015-07-08 11:16:07 +0300

    Create hlsplayer.sh

---
+++ hlsplayer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+url=$1
+lifara=/tmp/`date +%s`.fifo
+rm $lifara
+mkfifo $lifara
+mplayer $lifara &
+last=''
+while [ 1 == 1 ];do
+  u=`curl -s $url|tail -1`
+  if [ $u == $last ];then continue;fi
+  last=$u
+  curl -s $u >> $lifara
+  sleep 1
+done
